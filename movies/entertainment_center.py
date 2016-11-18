ed9498a4ee17adc7566748a5ee0edd57b23dfde9
Danny Teok <dannyteok@gmail.com>
2016-11-18 20:57:22 +0000

    Building Python Class for movies

---
+++ movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story"
+                        , "A story of a boy and his toy that comes to life"
+                        , "http://upload.wikimedia.org/blah"
+                        , "https://www.youtube.com/watch?v=vwyZblah")
+
+print(toy_story.trailer_youtube_url)
