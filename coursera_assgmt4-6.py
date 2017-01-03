e1fd6402969d54f89ca0a0dd43873a480d6a8d99
Danny Teok <danny.teok@smartfocus.com>
2017-01-03 14:35:47 +0000

    initial commit: coursera assignment 4.6. Done!

---
+++ coursera_assgmt4-6.py
@@ -0,0 +1,15 @@
+# Author: Danny Teok
+# Desc  : Coursera Assignment 4.6 - Functions
+
+
+def computepay(h, r):
+    if h <= 40.0:
+        return h * r
+    elif h > 40.0:
+        return (40 * r) + ((h - 40) * (r * 1.5))
+
+hrs = float(raw_input("Enter hours worked = "))
+rate = float(raw_input("Enter rates per hour = "))
+
+p = computepay(hrs, rate)
+print "Pay", p
