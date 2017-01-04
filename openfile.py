cf05646661a18a72b481bd55c62152f7b26c2d57
Danny Teok <dannyteok@gmail.com>
2017-01-04 22:41:24 +0000

    initial commit - reading data file

---
+++ openfile.py
@@ -0,0 +1,15 @@
+# Author: Danny Teok
+# Desc  : Opening and reading a data file
+# Notes : Developed in Python 2.7.14
+
+
+fhand = open('data/mbox.txt')
+
+
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From:'):
+        continue
+    print line
+
+
