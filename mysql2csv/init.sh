4cbb5b8499780217be0b1a21a1f9416d0f44344e
Danny Teok - (TeokD) <danny.teok@.umusic.com>
2017-04-11 09:28:15 +0100

    Fix aws cli upload syntax. Add comments to script.

--- mysql2csv/init.sh
+++ mysql2csv/init.sh
@@ -7,6 +7,7 @@ set -e
 bucketname="abcxyzchristest"
 region="eu-central-1"
 
+
 # Clear any potential old data
 rm -rf /opt/etl-dwh/outgoing/import
 rm -rf /opt/etl-dwh/outgoing/output
@@ -14,7 +15,12 @@ rm -rf /opt/etl-dwh/outgoing/output
 mkdir -p /opt/etl-dwh/outgoing/import
 mkdir -p /opt/etl-dwh/outgoing/output
 
+# Call python script
 python main_dbdump.py
 
-rm -rf output/*.tsv
-aws s3 cp --recursive --region $region s3://$bucketname/ output/
+rm -rf /opt/etl-dwh/outgoing/output/*.tsv
+
+# Upload to S3 bucket.
+# aws s3 cp --recursive <source> <s3: destination>
+# use --recursive if multiple files, otherwise drop it.
+aws s3 cp --recursive /opt/etl-dwh/outgoing/output s3://$bucketname/origin/
